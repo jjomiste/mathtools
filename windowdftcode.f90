21e0bb2db4831f5975206f356b3852debb97ce73
Juan J. Omiste <jomiste@ucm.es>
2025-04-23 12:35:55 +0200

    Including format to Windowed DFT

--- windowdftcode.f90
+++ windowdftcode.f90
@@ -6,7 +6,7 @@ program windowdft
   integer:: ifile,xcol,ycol,outfile, wdftfile
   real(8), allocatable :: x(:), y(:), omega(:), fre(:), fim(:)
   real(8), allocatable :: ywindow(:)
-  integer:: ij, jk, ik
+  integer:: ij, jk, ik,tstep
   real(8) :: windl!! duration of the window
   real(8) :: peak !! peak of the Hann window
   real(8) :: xx !! variable
@@ -23,6 +23,9 @@ program windowdft
 
   write(*,*) 'What is the duration of the window?'
   read(*,*) windl
+
+  write(*,*) 'Time steps in the to be stored'
+  read(*,*) tstep
 
   !!avoiding errors in the duration of the window
   if (windl.le.1d-3) then
@@ -45,11 +48,21 @@ program windowdft
 
   open(newunit=wdftfile, file=trim(outfilechar))
 
-  write(wdftfile,*) '#Duration of the file: ', windl
+  write(wdftfile,*) '#Input file: ', trim(infile)
   write(wdftfile,*) ''
+  write(wdftfile,*) '#Duration of the Hann Window: ', windl
+  write(wdftfile,*) ''
+
+  write(wdftfile,*) '# Note that the frequency units are the inverse of the input'
+  write(wdftfile,'(A2,3X, A1,17X,A1,16X,A13,3X,A7,10X,A7,10X,A9)') ' #',  't','f', 'omega(2*pi*f)',  &
+       'Re(FFT)',    'Im(FFT)',    '|WFFT|^2'
+  write(wdftfile,*) '# '
+
+  write(*,*) 'Computing the Windowed DFT'
+  write(*,*)
   
   !! LOOP IN THE TIME POINTS
-  Do ij=1,size(x)
+  Do ij=1,size(x),tstep
 
      peak=x(ij)
      !! Obtaining the time signal times the window
@@ -59,14 +72,12 @@ program windowdft
      call compute_wdft(x, ywindow,peak-windl*half,peak+windl*half, omega, fre, fim)
 
      do jk=1,size(omega)
-        write(wdftfile,*) x(ij), omega(jk)/(two*pi),omega(jk), fre(jk),fim(jk),fre(jk)**2.+fim(jk)**2.
+
+        write(wdftfile,'(6E17.5)') x(ij), omega(jk)/(two*pi),omega(jk), fre(jk),fim(jk),fre(jk)**2.+fim(jk)**2.
      end do
 
      write(wdftfile,*) ''
   End Do
  
-
-
-  
   
 end program windowdft
